6d9d0d37575c249e8ee2e4b1d83d475ef508ea0c
sdwoodbury <stuartwx@yahoo.com>
2015-11-19 16:15:58 -0500

    needs input validation and error handling

---
+++ app.py
@@ -0,0 +1,156 @@
+#!/usr/bin/python
+
+import app_lib, csv, json
+from bottle import route, run, request, static_file, get, post, template
+
+pk_var = {'instructor': 'i_id', 'course' : 'c_id', 'textbook' : 't_id', 'teaches':'i_id', 'additional' : 'a_id'}
+
+form_fields = {'course' : ['c_n', 'c_h', 'c_p'], 'instructor' : ['i_n', 'i_s'], 'textbook' : ['t_t', 't_n', 't_c'], 'teaches' : ['tc_id'], 'additional' : ['a_isbn']}
+
+columns = {'course' : ['course_name','credit_hours', 'primary_book'], 'instructor' : ['instructor_name', 'salary'], 'textbook' : ['title', 'author', 'cost'], 'teaches' : ['instructor_id'], 'additional' : ['isbn']} 
+
+primary_keys = {'course' : 'course_id','instructor' : 'instructor_id', 'textbook' : 'isbn', 'teaches' : 'course_id' , 'additional' : 'course_id'}
+
+def check_type(string):
+	try:
+		float(string)
+		return True
+	except ValueError:
+		try:
+			int(string)
+			return True
+		except ValueError:
+			return False
+
+@route('/')
+def home():
+	return static_file("home.html", root="./web")
+
+@post('/csv')
+def mass_upload():
+	inputs = ['instructor', 'course', 'textbook', 'teaches', 'additional']
+	return_string = 'success'
+	for item in inputs:
+		temp = request.forms.get(item)
+		if temp == '':
+			continue
+		try:
+			app_lib.bulk_load(item, temp)
+		except Exception as e:
+			return_string = e
+			
+	return template('./web/csv_result.html', data=[return_string])
+
+
+@post("/update")
+def update():
+	u_type = request.forms.get('type')
+	
+	temp = request.forms.get(pk_var[u_type])
+	if temp == '':
+		return template('.web/csv_result.html', data=["no primary key, no update"])
+	pk = str(temp)
+
+	string = ""
+	for i in range(0, len(form_fields[u_type])):
+		temp = request.forms.get(form_fields[u_type][i])
+		if temp != '':
+			string += columns[u_type][i] + ' = '
+			t = str(temp)
+		
+			if type(temp) == str:
+				t = "'" + temp + "'"
+			string += t + " "
+	
+	return_string = 'success'
+	try:
+		app_lib.update(u_type, string , primary_keys[u_type], pk)
+	except Exception as e:
+		return_string = e
+	return template('./web/csv_result.html', data=[return_string])
+
+@post('/insert')
+def insert():
+	u_type = request.forms.get('type')
+	
+	temp = [request.forms.get(pk_var[u_type])]
+	if temp[0] == '':
+		return template('.web/csv_result.html', data=["no primary key, no insert"])
+	pk = str(temp)
+
+	for i in range(0, len(form_fields[u_type])):
+		if type(i) == str:
+			temp.append("'" + str(request.forms.get(form_fields[u_type][i])) + "'")
+		else:
+			temp.append(request.forms.get(form_fields[u_type][i]))
+
+	return_string = 'success'
+	
+	try:
+		app_lib.insert(temp, u_type)
+	except Exception as e:
+		return_string = e
+	return template('./web/csv_result.html', data=[return_string])
+
+
+@post('/delete')
+def delete():
+	u_type = request.forms.get('type')
+	pk_var1 = {'instructor': ['i_id'], 'course' : ['c_id'], 'textbook' : ['t_id'], 'teaches':['i_id', 'c_id'], 'additional' : ['a_id', 'a_isbn']}
+	
+	primary_keys1 = {'course' : ['course_id'],'instructor' : ['instructor_id'], 'textbook' : ['isbn'], 'teaches' : ['course_id', 'instructor_id'] , 'additional' : ['course_id', 'isbn']}
+	
+	temp = ''
+	for i in range(0, len(pk_var1[u_type])):
+		temp += primary_keys1[u_type][i] + ' = '
+		if type(i) == str:
+			temp += "'" + request.forms.get(pk_var1[u_type][i]) + "', "
+		else:
+			temp += request.forms.get(pk_var1[u_type][i]) + ", "
+		
+	
+	temp = temp[:-2]
+	return_string = 'success'
+	try:
+		app_lib.delete(temp, u_type)
+	except Exception as e:
+		return_string = e
+	return template('./web/csv_result.html', data=[return_string])
+
+@post('/select')
+def select():
+	u_type = request.forms.get('type')
+	
+	fields = form_fields[u_type][:]
+	fields.insert(0, pk_var[u_type])
+
+	cols = columns[u_type][:]
+	cols.insert(0, primary_keys[u_type])
+	string = ''
+	for i in range(0, len(fields)):
+		temp = request.forms.get(fields[i])
+		if temp != '':
+			if check_type(temp) == False:
+				string += cols[i] + " = '" + temp + "'and "
+			else:
+				string += cols[i] + ' = ' + str(temp) + 'and '
+
+	string = string[:-4]
+
+	return_string = 'no_call'
+	try:
+		data = app_lib.select(string, u_type)
+		return_list = []
+		for item in data:
+			return_string = ''
+			for entry in item:
+				return_string += str(entry) + ' '
+			return_list.append(return_string)
+	except Exception as e:
+		return_string = e
+	return template('./web/select_result.html', data=[return_list])
+
+run(host='localhost', port=8080, debug=True)
+
+app_lib.close()
+
--- app_lib.py
+++ app_lib.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3.4
+#!/usr/bin/python
 
 import sqlite3, csv
 
@@ -7,13 +7,6 @@ cur = conn.cursor()
 
 #conn.isolation_level="EXCLUSIVE" #now have to commit transactions
 conn.isolation_level=None #now autocommit. (default)
-
-#returns list of primary keys for table
-def get_pk(table):
-	cur.execute("pragma table_info(" + table + ")")
-	primary_key = cur.fetchall()
-
-	return [x[1] for x in [list(y) for y in primary_key] if x[-1] == 1] #may be dealing with composite keys
 
 #returns 1 for good, 0 for bad
 #it is bad if there is a course id in course which is not in teaches
@@ -42,9 +35,8 @@ def insert(values, table):
 #values is a list
 #the table had better have a primary key
 #better verify that the values aren't sql injections
-def delete(values, table):
-	pk = get_pk(table)
-	[cur.execute("delete from " + table + " where " + str(pk[0]) + "=" + str(x) + ';') for x in values]
+def delete(string, table):
+	cur.execute("delete from " + table + " where " + string)
 
 #assumes all parameters are not lists
 def update(table, string, pk, pk_val):
@@ -57,30 +49,17 @@ def bulk_load(table, csv_file):
 		reader = csv.reader(x, delimiter=',')
 		[insert(y, table) for y in reader]
 
+#selects tuples matching constraint, returns a list
+def select(string, table):
+	cur.execute("select * from  " + table + " where " + string + ";")
+	foo = list(cur.fetchall())
+	ret = []
+	for item in foo:
+		ret.append(list(item))
+	return ret
+
 def close():
 	conn.close()
 
-'''
-bulk_load("textbook", "book.csv")
-cur.execute("select * from textbook;")
-print(cur.fetchall())
 
-insert([1, 'a', 1], "course")
-cur.execute("select * from course;")
-print(cur.fetchall())
-update("course", "course_name", "b", 1)
-cur.execute("select * from course;")
-print(cur.fetchall())
-delete([1], "course")
-cur.execute("select * from course;")
-print(cur.fetchall())
-'''
-#cur.execute("select * from course;")
-#print(cur.fetchall())
-#cur.execute('select * from course;')
-#print(cur.fetchall())
-
-#print(check_teaches(),"\n",check_has())
-
-#conn.close()
 
